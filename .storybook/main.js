bee34fbec3f3c74ede2d058de08ff28c57fc4df8
gn-t-k <gntk1995422@gmail.com>
2021-12-02 06:22:53 +0900

    storybook addon追加

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,7 +2,11 @@ const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
 
 module.exports = {
   stories: ["../src/**/*.stories.tsx"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "storybook-addon-next-router",
+  ],
   core: {
     builder: "webpack5",
   },
